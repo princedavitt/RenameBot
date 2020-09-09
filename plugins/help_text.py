acc38973e189aa4ffbb7bcc31255351d12345f83
Mr Robot <jokerhacker.6521@protonmail.com>
2020-09-10 01:26:54 +0530

    small changes in bot help menu

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -70,12 +70,13 @@ async def start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/jkbotupdates'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/MrRobot222')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/jkbotupdates
+                                         '),
+                    InlineKeyboardButton('Source', url='https://github.com/MrRobot222/RenameBot')
                 ]
             ]
         ),
